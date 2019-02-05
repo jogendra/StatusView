616984afd705ded010b2ac00b03db9d5ceb20452
Jogendra Singh <imjog24@gmail.com>
2019-02-05 19:45:04 +0530

    Initial UI setups

---
+++ Example/StatusView/StatusView.swift
@@ -0,0 +1,85 @@
+//
+//  StatusView.swift
+//  StatusView_Example
+//
+//  Created by JOGENDRA on 05/02/19.
+//  Copyright © 2019 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+protocol StatusViewDelegate: class {
+    func didTapStatusView()
+}
+
+class StatusView: UIView {
+
+    weak var delegate: StatusViewDelegate?
+
+    lazy var textLabel = UILabel()
+
+    public var statusViewHeight: CGFloat = 28
+    public var titleTextColor: UIColor = .white
+    public var titleTextFontSize: CGFloat = 12
+    public var titleText: String = ""
+
+    init() {
+        addTapGuestureToStatusView()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func addTapGuestureToStatusView() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.didTapStatusView))
+        self.addGestureRecognizer(tapGesture)
+    }
+
+    @objc func didTapStatusView() {
+        delegate?.didTapStatusView()
+    }
+
+    func addLabelWithAnimation() {
+        textLabel = UILabel(frame: CGRect(x: 0, y: UIApplication.shared.statusBarFrame.height, width: UIApplication.shared.statusBarFrame.width, height: statusViewHeight))
+        self.addSubview(textLabel)
+
+        textLabel.translatesAutoresizingMaskIntoConstraints = false
+        textLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 38).isActive = true
+        textLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+        textLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        textLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        textLabel.text = titleText
+        textLabel.textColor = titleTextColor
+        textLabel.textAlignment = .center
+        textLabel.backgroundColor = UIColor.clear
+        textLabel.font = UIFont.systemFont(ofSize: titleTextFontSize)
+        addTextLabelAnimations()
+    }
+
+    func addTextLabelAnimations() {
+        textLabel.addPulseAnimation(from: 0.4, to: 1, duration: 0.8, key: "opacity")
+    }
+
+    public func updateStatusView(statusViewColor: UIColor?, text: String?, textColor: UIColor?) {
+        self.backgroundColor = statusViewColor
+        textLabel.text = text
+        textLabel.textColor = textColor ?? titleTextColor
+    }
+
+}
+
+extension UIView {
+
+    func addPulseAnimation(from fromValue: Any, to toValue: Any, duration: CFTimeInterval, key: String) {
+        let pulseAnimation = CABasicAnimation(keyPath: key)
+        pulseAnimation.duration = duration
+        pulseAnimation.fromValue = fromValue
+        pulseAnimation.toValue = toValue
+        pulseAnimation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
+        pulseAnimation.autoreverses = true
+        pulseAnimation.repeatCount = .greatestFiniteMagnitude
+        layer.add(pulseAnimation, forKey: key)
+    }
+
+}
